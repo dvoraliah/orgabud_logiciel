7898b6ec1d3cd3932d78cfb4899ea3daa447aa42
dvoraliah <dvoraliah@gmail.com>
2022-08-08 22:43:43 +0200

    Recuperation des id des lignes pour les buttons

--- loggedApp.py
+++ loggedApp.py
@@ -7,6 +7,7 @@ class MainApp(QWidget):
     def __init__(self, token):
         super().__init__()
 
+        self.id_obj = None
         self.button_delete = None
         self.button_edit = None
         self.tableWidget = None
@@ -38,11 +39,13 @@ class MainApp(QWidget):
             self.tableWidget.setItem(i, 1, QTableWidgetItem(self.response_json[i]["name"]))
             self.tableWidget.setItem(i, 2, QTableWidgetItem(self.response_json[i]["email"]))
             self.button_edit = QPushButton('edit')
+            self.button_edit.id_obj = self.response_json[i]["id"]
             self.button_edit.button_row = i
             self.button_edit.button_column = 3
             self.button_edit.clicked.connect(self.button_update_clicked)
             self.tableWidget.setCellWidget(i, 3, self.button_edit)
             self.button_delete = QPushButton('suppr')
+            self.button_delete.id_obj = self.response_json[i]["id"]
             self.button_delete.button_row = i
             self.button_delete.button_column = 4
             self.button_delete.clicked.connect(self.button_delete_clicked)
@@ -53,12 +56,15 @@ class MainApp(QWidget):
 
         # button.show()
 
-    def button_update_clicked(self):
+    def button_update_clicked(self, arg):
         button = self.sender()
         print(button.button_row)
         print(button.button_column)
+        print(button.id_obj)
+        print(arg)
 
     def button_delete_clicked(self):
         button = self.sender()
         print(button.button_row)
         print(button.button_column)
+        print(button.id_obj)
