b53a95362ecf76df62d17a865d70f92d19deff8e
dvoraliah <dvoraliah@gmail.com>
2022-08-09 20:40:49 +0200

    Function delete fonctionnelle et ajout de la colonne de status de l'utilisateur dans le tableau d'affichage

--- loggedApp.py
+++ loggedApp.py
@@ -1,4 +1,5 @@
 import requests
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import QLabel, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QPushButton
 from requests.structures import CaseInsensitiveDict
 
@@ -7,6 +8,7 @@ class MainApp(QWidget):
     def __init__(self, token):
         super().__init__()
 
+        self.window = None
         self.id_obj = None
         self.button_delete = None
         self.button_edit = None
@@ -30,20 +32,28 @@ class MainApp(QWidget):
     def data_users(self):
         self.response_API = requests.get(self.users_request, headers=self.headers)
         self.response_json = self.response_API.json()
-        self.tableWidget = QTableWidget(len(self.response_json), 5)
-        self.tableWidget.setHorizontalHeaderLabels(['id', 'Pseudo', 'Email', 'Editer', 'X'])
+        self.tableWidget = QTableWidget(len(self.response_json), 6)
+        self.tableWidget.setHorizontalHeaderLabels(['id', 'Pseudo', 'Email', 'Statut', 'Editer', 'X'])
         self.tableWidget.horizontalHeader().setDefaultSectionSize(300)
 
         for i in range(len(self.response_json)):
+            if self.response_json[i]["status_id"] == 1:
+                status = "User"
+            elif self.response_json[i]["status_id"] == 2:
+                status = "Premium"
+            else:
+                status = "Admin"
+
             self.tableWidget.setItem(i, 0, QTableWidgetItem(str(self.response_json[i]["id"])))
             self.tableWidget.setItem(i, 1, QTableWidgetItem(self.response_json[i]["name"]))
             self.tableWidget.setItem(i, 2, QTableWidgetItem(self.response_json[i]["email"]))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(status))
             self.button_edit = QPushButton('edit')
-            self.button_edit.id_obj = self.response_json[i]["id"]
+            self.button_edit.id_obj = str(self.response_json[i]["id"])
             self.button_edit.button_row = i
-            self.button_edit.button_column = 3
+            self.button_edit.button_column = 4
             self.button_edit.clicked.connect(self.button_update_clicked)
-            self.tableWidget.setCellWidget(i, 3, self.button_edit)
+            self.tableWidget.setCellWidget(i, 5, self.button_edit)
             self.button_delete = QPushButton('suppr')
             self.button_delete.id_obj = self.response_json[i]["id"]
             self.button_delete.button_row = i
@@ -53,6 +63,7 @@ class MainApp(QWidget):
         self.tableWidget.setColumnWidth(0, 20)
         self.tableWidget.setColumnWidth(3, 80)
         self.tableWidget.setColumnWidth(4, 80)
+        self.tableWidget.setColumnWidth(5, 80)
 
         # button.show()
 
@@ -65,6 +76,11 @@ class MainApp(QWidget):
 
     def button_delete_clicked(self):
         button = self.sender()
-        print(button.button_row)
-        print(button.button_column)
-        print(button.id_obj)
+        api_url = 'http://5.39.76.125/dvoraliah/orgabud-website/public/index.php/api/users/' + str(button.id_obj)
+        response_API = requests.delete(api_url, headers=self.headers)
+        response_statut = response_API.status_code
+        response_json = response_API.json()
+        print("delete")
+        self.tableWidget.removeRow(button.button_row)
+        self.data_users()
+        print("success")
